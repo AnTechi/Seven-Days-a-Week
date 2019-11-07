8b567bdb6411cb59920c645ba5d47cb616e3c31c
Tushar <tushar199814@gmail.com>
2019-11-07 23:36:02 +0530

    Dialog box for gender

--- app/src/main/java/com/example/sevendaysaweek/Signup_user.java
+++ app/src/main/java/com/example/sevendaysaweek/Signup_user.java
@@ -1,7 +1,9 @@
 package com.example.sevendaysaweek;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 public class Signup_user extends AppCompatActivity {
 
     public TextView gender_txt;
+    private String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,43 @@ public class Signup_user extends AppCompatActivity {
 
     public void Display_genderdialog(View view)
     {
-        new Gender_dialog().show(getSupportFragmentManager(),"genderdialog");
+        GetGender();
+
+    }
+    public void GetGender()
+    {
+
+        final String[] gen=this.getResources().getStringArray(R.array.genderarr);
+       AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Select your Gender");
+        builder.setSingleChoiceItems(R.array.genderarr, -1, new DialogInterface.OnClickListener() {
+                    @Override
+
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        gender=gen[i];
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        gender_txt.setText(gender);
+
+
+                    }
+                });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
+
+
+
 
 
     }
