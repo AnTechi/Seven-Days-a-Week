4e0bee933ececcf4102c821a8d72a93616e7869e
Tushar <tushar199814@gmail.com>
2019-11-17 14:08:45 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/5.4.1/executionHistory/executionHistory.bin
    #	.gradle/5.4.1/executionHistory/executionHistory.lock
    #	.gradle/5.4.1/fileHashes/fileHashes.bin
    #	.gradle/5.4.1/fileHashes/fileHashes.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	app/src/main/java/com/example/sevendaysaweek/Gender_dialog.java

---
+++ app/src/main/java/com/example/sevendaysaweek/LoginActivity.java
@@ -0,0 +1,141 @@
+package com.example.sevendaysaweek;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText LoginEmail,LoginPassword;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser User;
+    private Toolbar toolbar;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__avtivity);
+        LoginEmail=findViewById(R.id.Login_Email);
+        LoginPassword=findViewById(R.id.Login_Password);
+        progressDialog=new ProgressDialog(this);
+        firebaseAuth=firebaseAuth.getInstance();
+
+        User=firebaseAuth.getCurrentUser();
+        toolbar=findViewById(R.id.toolbar);
+        toolbar.setTitle("Login");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Login buttonClicked
+    public void LoginUser(View view)
+    {
+
+        UserLogin();
+    }
+
+
+    //Firebase Login Steps
+    //Checking email and password
+    private void UserLogin() {
+        String email = LoginEmail.getText().toString().trim();
+        String password = LoginPassword.getText().toString().trim();
+
+        //checking if email and password is empty..
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(email.matches(emailPattern)){
+
+            progressDialog.setMessage("Loging in.....");
+            progressDialog.show();
+
+
+            //email verification mail sending using firebase..
+            if (User.isEmailVerified()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        //open main activity if login is successful..
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            LoginActivity.this.startActivity(intent);
+
+                            Toast.makeText(LoginActivity.this, "Login succesful", Toast.LENGTH_LONG).show();
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Incorrect Email or Password ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+            } else {
+                progressDialog.dismiss();
+                Toast.makeText(LoginActivity.this, "not verified", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        else
+        {
+            Toast.makeText(LoginActivity.this, "Incorrect Email", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+    //forgot password button click
+    //opening reset password activity..
+    public void ForgotPassword(View view)
+    {
+        startActivity(new Intent(this,ResetPassword_activity.class));
+
+
+    }
+}
