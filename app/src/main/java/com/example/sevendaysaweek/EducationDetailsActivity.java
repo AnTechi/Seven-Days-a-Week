af76e7865805c8628ce0a713eda46ba9ce3d4b51
Tushar <tushar199814@gmail.com>
2019-11-17 19:37:57 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/5.4.1/executionHistory/executionHistory.bin
    #	.gradle/5.4.1/executionHistory/executionHistory.lock
    #	.gradle/5.4.1/fileHashes/fileHashes.bin
    #	.gradle/5.4.1/fileHashes/fileHashes.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	app/src/main/java/com/example/sevendaysaweek/Gender_dialog.java

---
+++ app/src/main/java/com/example/sevendaysaweek/EducationDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.sevendaysaweek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EducationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education_details_activity);
+    }
+}
